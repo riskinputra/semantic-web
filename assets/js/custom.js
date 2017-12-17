54342e9eeb6e7ff5cc9c440a34b02dbfdd767ae5
riskinputra <riski.n.putra@gmail.com>
2017-12-17 08:02:28 +0700

    contact-1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,3 +7,10 @@ function scrollFunction() {
     document.getElementById("scroll-up").style.display = "none";
   }
 }
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: {lat: -6.21462, lng: 106.84513}
+  });
+}
